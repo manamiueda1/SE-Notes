74b0776148b3ce507fe7f65710fa45875133e746
Manami Ueda <manamiueda1@gmail.com>
2021-01-04 15:56:06 -0500

    added calc html css

---
+++ homework.js
@@ -0,0 +1,53 @@
+// Problem 1
+
+// Using Math.random(), console.log 3 random numbers from 5 to 15
+
+// Problem 2
+
+// Define a variable name and assign your name to it
+// Create a function that returns your name
+
+// Problem 3
+
+// Create a function that accepts 2 arguments, both of which are numbers, and returns the sum of those two numbers
+
+// Problem 4
+
+// Create a function that returns the type of any argument that is being called in the function
+// example
+// type(400) // => 'number'
+// type('hi') // => 'string'
+
+// Problem 5
+// Hint: Ensure you are modifying the parameter and not the original variable inside the function, this will make your life easier with this question
+
+// var fruits = ['apple', 'orange', 'peach', 'lemon', 'plum', 'grape', 'watermelon'];
+
+// Using the array above, create a funciton that returns the 4th element in the array.
+
+// Create a function that returns only the last element in the fruits array
+
+// Create a function that returns [peach', 'lemon', 'plum', 'grape']
+
+// Create a function that adds another furit to the end of the array and returns the new array
+
+// Create a funciton that returns the string, 'apple and orange and peach and lemon and plum and grape and watermelon'
+
+// Problem 6
+
+// var names = ['Wil', 'Shamika', 'Samuel', 'Ramsey']
+
+// Using the array above, create a fucntion that returns the string, 'Hello Wil, happy Friday!'
+
+// Create a funciton that returns the string, 'Hi Shamika and Ramsey!'
+
+// Create a function that returns the string, 'Howdy Sam!'
+// Note: Be sure to change 'Samuel' to 'Sam'
+
+// Problem 7
+
+// var nums = [12, 3, 50, 1, 99, 2]
+
+// Using a while loop, create a funciton that adds every number in the nums array and return the total number
+// Note: this one is very challenging and requires a little bit of research, but give it shot!
+// And remember to control - c if you log to the terminal and it is running infinitely!
