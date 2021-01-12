daf375895c08ccbd0c80622e74c05adf3272be3f
Manami Ueda <manamiueda1@gmail.com>
2021-01-11 20:12:12 -0500

    aded DOM notes

---
+++ notes/DOM/DOM-Intro/script.js
@@ -0,0 +1,30 @@
+// target the body
+// let body = document.body;
+let allH1s = document.querySelectorAll('h1');
+
+addAllListeners(allH1s);
+
+function addAllListeners(param){
+    for(let i = 0; i < param.length; i++){
+        if( param[i].innerHTML === "World" ){
+            param[i].addEventListener('click', change);
+        } else {
+            param[i].addEventListener('click', hit);
+        }
+    }
+}
+
+function hit(event){
+    console.log(event.target);
+}
+
+function change(){
+    console.log('changing');
+}
+
+
+console.log(allH1s);
+
+// console.log(document)
+// console.log(event)
+// console.log(event.target)
